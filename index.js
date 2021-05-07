8c4640b6cca8eb7bb53d7ed86a12d84c4afe05e4
Tristan <tantris.kantner@gmail.com>
2021-05-07 12:51:38 +0200

    Add Files

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 // eslint-disable-next-line unicorn/prefer-node-protocol
 const http = require('http');
+// eslint-disable-next-line unicorn/prefer-node-protocol
+const fs = require('fs');
 
 const host = 'localhost';
-const port = 8000;
+const port = 3000;
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('My first server!');
+  let sitePath = '/Public' + req.url;
+  if (sitePath === '/Public/') {
+    sitePath = '/Public/index.html';
+  }
+  console.log(__dirname + sitePath);
+  fs.readFile(__dirname + sitePath, function (error, content) {
+    if (error) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.end('Site Not Found');
+    } else {
+      res.writeHead(200);
+      res.end(content);
+    }
+  });
 };
 
 const server = http.createServer(requestListener);
+
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
